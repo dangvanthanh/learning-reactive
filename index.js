9ea61ac608392d64cae4518344030fd2cbb8b486
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-03-27 22:02:04 +0700

    Initialize rxjs

---
+++ index.js
@@ -0,0 +1,7 @@
+// Purity
+var button = document.querySelector('button')
+
+Rx.Observable.fromEvent(button, 'click')
+  .throttleTime(1000)
+  .scan(count => count + 1, 0)
+  .subscribe(count => console.log(`Clicked ${count} times`))
